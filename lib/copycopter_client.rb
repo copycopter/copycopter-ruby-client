d8db2c8b545d23aca4830ea51faabacb68e56741
Joe Ferris <joe.r.ferris@gmail.com>
2010-10-08 17:37:02 -0400

    Updated documentation

--- lib/copycopter_client.rb
+++ lib/copycopter_client.rb
@@ -1,15 +1,46 @@
 require 'copycopter_client/version'
 require 'copycopter_client/configuration'
 
-# Plugin for applications to store their copy in a remote service to be editable by clients
+# Top-level interface to the Copycopter client.
+#
+# Most applications should only need to use the {.configure}
+# method, which will setup all the pieces and begin synchronization when
+# appropriate.
 module CopycopterClient
+  # Added to all log entries created from the client
   LOG_PREFIX = "** [Copycopter] "
 
   class << self
+    # @return [Client] instance used to communicate with the Copycopter server.
+    # This is set when {.configure} is called.
     attr_accessor :client
+
+    # @return [Configuration] current client configuration
+    # Must act like a hash and return sensible values for all Copycopter
+    # configuration options. Usually set when {.configure} is called.
+    attr_accessor :configuration
   end
 
+  # Issues a new deploy, marking all draft blurbs as published.
+  # This is called when the copycopter:deploy rake task is invoked.
   def self.deploy
     client.deploy
+  end
+
+  # Call this method to modify defaults in your initializers.
+  #
+  # @example
+  #   CopycopterClient.configure do |config|
+  #     config.api_key = '1234567890abcdef'
+  #     config.secure  = false
+  #   end
+  #
+  # @param apply [Boolean] (internal) whether the configuration should be applied yet.
+  #
+  # @yield [Configuration] the configuration to be modified
+  def self.configure(apply = true)
+    self.configuration ||= Configuration.new
+    yield(configuration)
+    configuration.apply if apply
   end
 end
