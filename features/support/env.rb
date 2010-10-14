11fc5a15f02f6576ba7ff15823358eb80b3a3d47
Joe Ferris <joe.r.ferris@gmail.com>
2010-10-14 13:01:18 -0400

    Improved cucumber rails server

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,3 +6,13 @@ PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 $LOAD_PATH << File.join(PROJECT_ROOT, 'lib')
 require "copycopter_client/version"
 
+Before do
+  if ENV['debug']
+    @puts = true
+    @announce_stdout = true
+    @announce_stderr = true
+    @announce_cmd = true
+    @announce_dir = true
+    @announce_env = true
+  end
+end
