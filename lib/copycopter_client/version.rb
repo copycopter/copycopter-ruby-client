4e343fc96cfd8a6d94b224a0d5d1d56139ca24b7
Jason Morrison <jmorrison@thoughtbot.com>
2011-09-07 13:50:20 -0400

    Don't freeze VERSION, rubygems under 1.9 calls VERSION.strip!

--- lib/copycopter_client/version.rb
+++ lib/copycopter_client/version.rb
@@ -1,6 +1,6 @@
 module CopycopterClient
   # Client version
-  VERSION = "1.0.4".freeze
+  VERSION = "1.0.4"
 
   # API version being used to communicate with the server
   API_VERSION = "2.0".freeze
