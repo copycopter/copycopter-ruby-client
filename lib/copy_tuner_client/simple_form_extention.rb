3808d2284ef2b490e7267486da415bb69b528fb8
Akihiro Matsumura <matsumura.aki@gmail.com>
2014-06-30 13:56:10 +0900

    Fix error on ransack form

--- lib/copy_tuner_client/simple_form_extention.rb
+++ lib/copy_tuner_client/simple_form_extention.rb
@@ -11,12 +11,15 @@ if defined?(SimpleForm)
     def label_translation_with_copyray_comment
       source = label_translation_without_copyray_comment
 
-      attributes_scope = "#{object.class.i18n_scope}.attributes"
-      defaults = object.class.lookup_ancestors.map do |klass|
-        "#{attributes_scope}.#{klass.model_name.i18n_key}.#{reflection_or_attribute_name}"
+      if object.class.respond_to?(:lookup_ancestors)
+        attributes_scope = "#{object.class.i18n_scope}.attributes"
+        defaults = object.class.lookup_ancestors.map do |klass|
+          "#{attributes_scope}.#{klass.model_name.i18n_key}.#{reflection_or_attribute_name}"
+        end
+        CopyTunerClient::Copyray.augment_template(source, defaults.shift)
+      else
+        source
       end
-
-      CopyTunerClient::Copyray.augment_template(source, defaults.shift)
     end
     alias_method_chain :label_translation, :copyray_comment
   end
