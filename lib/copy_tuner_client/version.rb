25967cde05f843010ec43ac88138d68b3324315c
Akihiro Matsumura <matsumura.aki@gmail.com>
2015-02-18 12:10:38 +0900

    Version up 0.1.1.beta17

--- lib/copy_tuner_client/version.rb
+++ lib/copy_tuner_client/version.rb
@@ -1,6 +1,6 @@
 module CopyTunerClient
   # Client version
-  VERSION = '0.1.1.beta16'
+  VERSION = '0.1.1.beta17'
 
   # API version being used to communicate with the server
   API_VERSION = '2.0'.freeze
