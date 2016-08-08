5b4052d6cea5efd509ec866b2608f498430989a0
Shunichi Ikegami <sike.tm@gmail.com>
2016-08-08 10:53:18 +0900

    Bump version to 0.3.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.3.1
+- Add search box to copyray bar.
+- Add disable_copyray_comment_injection to configuration.
+
 ## 0.3.0
 - Use https as default.
 - Download blurbs from S3.
--- lib/copy_tuner_client/version.rb
+++ lib/copy_tuner_client/version.rb
@@ -1,6 +1,6 @@
 module CopyTunerClient
   # Client version
-  VERSION = '0.3.0'.freeze
+  VERSION = '0.3.1'.freeze
 
   # API version being used to communicate with the server
   API_VERSION = '2.0'.freeze
