f6db487b100c52034f26fa2cdaa9ca06d496824d
Shunichi Ikegami <sike.tm@gmail.com>
2017-02-10 14:04:47 +0900

    Hook methods after load_config_initializers

--- lib/copy_tuner_client/engine.rb
+++ lib/copy_tuner_client/engine.rb
@@ -6,7 +6,9 @@ module CopyTunerClient
   class Engine < ::Rails::Engine
     initializer :initialize_copy_tuner_rails, :before => :load_config_initializers do |app|
       CopyTunerClient::Rails.initialize
+    end
 
+    initializer :initialize_copy_tuner_hook_methods, :after => :load_config_initializers do |app|
       ActiveSupport.on_load(:action_view) do
         ActionView::Helpers::TranslationHelper.class_eval do
           def translate_with_copyray_comment(key, options = {})
@@ -27,6 +29,8 @@ module CopyTunerClient
       if CopyTunerClient.configuration.enable_middleware?
         CopyTunerClient::TranslationLog.install_hook
       end
+
+      require 'copy_tuner_client/simple_form_extention'
     end
 
     initializer "copy_tuner.assets.precompile", group: :all do |app|
