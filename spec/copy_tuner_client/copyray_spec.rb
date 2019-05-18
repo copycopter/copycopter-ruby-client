aeb8c42d47fbaef4d4e0d9d7fdc8249bfc3ea30e
aki <lala.akira@gmail.com>
2019-05-18 20:10:21 +0900

    Add CopyTunerClient::Copyray test

---
+++ spec/copy_tuner_client/copyray_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+require 'copy_tuner_client/copyray'
+
+describe CopyTunerClient::Copyray do
+  describe '.augment_template' do
+    subject { CopyTunerClient::Copyray.augment_template(source, key) }
+
+    let(:key) { 'en.test.key' }
+
+    shared_examples 'Not escaped' do
+      it { is_expected.to be_html_safe }
+      it { is_expected.to eq "<!--COPYRAY #{key}--><b>Hello</b>" }
+    end
+
+    context 'html_escape option is false' do
+      before do
+        CopyTunerClient.configure do |configuration|
+          configuration.html_escape = false
+        end
+      end
+
+      context 'string not marked as html safe' do
+        let(:source) { FakeHtmlSafeString.new('<b>Hello</b>') }
+
+        it_behaves_like 'Not escaped'
+      end
+
+      context 'string marked as html safe' do
+        let(:source) { FakeHtmlSafeString.new('<b>Hello</b>').html_safe }
+
+        it_behaves_like 'Not escaped'
+      end
+    end
+
+    context 'html_escape option is true' do
+      before do
+        CopyTunerClient.configure do |configuration|
+          configuration.html_escape = true
+        end
+      end
+
+      context 'string not marked as html safe' do
+        let(:source) { FakeHtmlSafeString.new('<b>Hello</b>') }
+
+        it { is_expected.to be_html_safe }
+        it { is_expected.to eq "<!--COPYRAY #{key}-->&lt;b&gt;Hello&lt;/b&gt;" }
+      end
+
+      context 'string marked as html safe' do
+        let(:source) { FakeHtmlSafeString.new('<b>Hello</b>').html_safe }
+
+        it_behaves_like 'Not escaped'
+      end
+    end
+  end
+end
