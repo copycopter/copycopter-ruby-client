831aa4ecf6791628cd3ba8364196c64a89143745
aki <lala.akira@gmail.com>
2019-05-22 18:52:14 +0900

    bump 0.4.12

--- lib/copy_tuner_client/version.rb
+++ lib/copy_tuner_client/version.rb
@@ -1,6 +1,6 @@
 module CopyTunerClient
   # Client version
-  VERSION = '0.4.11'.freeze
+  VERSION = '0.4.12'.freeze
 
   # API version being used to communicate with the server
   API_VERSION = '2.0'.freeze
