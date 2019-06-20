30eb3f284fcd0759f2f16abfb4dd957384c3d309
aki <lala.akira@gmail.com>
2019-06-20 18:38:02 +0900

    chore: 0.5.0

--- lib/copy_tuner_client/version.rb
+++ lib/copy_tuner_client/version.rb
@@ -1,6 +1,6 @@
 module CopyTunerClient
   # Client version
-  VERSION = '0.5.0.pre'.freeze
+  VERSION = '0.5.0'.freeze
 
   # API version being used to communicate with the server
   API_VERSION = '2.0'.freeze
