7dca4ed4e03f9ce1141f2164bc14887210e9cbff
Shunichi Ikegami <sike.tm@gmail.com>
2021-09-20 11:24:12 +0900

    chore: Test against rails 6.1 and master

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+BUNDLE_GEMFILE=gemfiles/5.2.gemfile bundle install && BUNDLE_GEMFILE=gemfiles/5.2.gemfile bundle exec rspec spec
+BUNDLE_GEMFILE=gemfiles/6.0.gemfile bundle install && BUNDLE_GEMFILE=gemfiles/6.0.gemfile bundle exec rspec spec
+BUNDLE_GEMFILE=gemfiles/6.1.gemfile bundle install && BUNDLE_GEMFILE=gemfiles/6.1.gemfile bundle exec rspec spec
+BUNDLE_GEMFILE=gemfiles/master.gemfile bundle install && BUNDLE_GEMFILE=gemfiles/master.gemfile bundle exec rspec spec
