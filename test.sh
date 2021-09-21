39dd704cee030aca1836660433da9d72e9ef7414
Shunichi Ikegami <sike.tm@gmail.com>
2021-09-21 10:17:50 +0900

    fix: gemfile for rails main branch

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 BUNDLE_GEMFILE=gemfiles/5.2.gemfile bundle install && BUNDLE_GEMFILE=gemfiles/5.2.gemfile bundle exec rspec spec
 BUNDLE_GEMFILE=gemfiles/6.0.gemfile bundle install && BUNDLE_GEMFILE=gemfiles/6.0.gemfile bundle exec rspec spec
 BUNDLE_GEMFILE=gemfiles/6.1.gemfile bundle install && BUNDLE_GEMFILE=gemfiles/6.1.gemfile bundle exec rspec spec
-BUNDLE_GEMFILE=gemfiles/master.gemfile bundle install && BUNDLE_GEMFILE=gemfiles/master.gemfile bundle exec rspec spec
+BUNDLE_GEMFILE=gemfiles/main.gemfile bundle install && BUNDLE_GEMFILE=gemfiles/main.gemfile bundle exec rspec spec
